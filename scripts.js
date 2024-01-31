c42042c34dbad44d8a24aff2a6df6cd27440afb9
noviso123 <jtsatiro@hotmail.com>
2024-01-31 14:10:27 -0300

    Produção_V1
    31/10/2024 - 14:10

---
+++ scripts.js
@@ -0,0 +1,42 @@
+// Google tag (gtag.js)
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-KGLV1576M0');
+
+// Event snippet for Rota conversion page
+gtag('event', 'conversion', { 'send_to': 'AW-862283090/e01VCKSky40ZENLKlZsD' });
+
+// Event snippet for FinalizaçãoDeCompra conversion page
+gtag('event', 'conversion', { 'send_to': 'AW-862283090/iFwVCPnRko0ZENLKlZsD' });
+
+// Melhoria na interação usando JavaScript
+// Função para ação de compra
+function comprar() {
+    // Código para a ação de compra (pode ser adicionado aqui)
+
+    // Redirecionamento para o novo link da Eduzz
+    window.location.href = "https://bit.ly/VendasEm48H";
+}
+
+// Função para relatar a conversão ao clicar no botão de compra
+function gtag_report_conversion(url) {
+    var callback = function () {
+        if (typeof (url) != 'undefined') {
+            window.location = url;
+        }
+    };
+    gtag('event', 'conversion', {
+        'send_to': 'AW-862283090/iFwVCPnRko0ZENLKlZsD',
+        'event_callback': callback
+    });
+    return false;
+}
+
+// Adicione o script para Google AdSense
+// Note: Este script já está incluído no HTML, este bloco apenas ilustra a referência ao AdSense
+// Certifique-se de configurar o AdSense corretamente para o seu caso
+// Código do AdSense:
+// <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9013233807727287" crossorigin="anonymous"></script>
